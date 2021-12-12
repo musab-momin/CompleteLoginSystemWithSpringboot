f2d8a054efaefdcda3b0dc4d9acc2312a84d7a37
Musab Momin <musab.momin@kapso.in>
2021-12-12 14:03:06 +0530

    registration front end done

--- src/main/java/com/reglog/controllers/RegistrationController.java
+++ src/main/java/com/reglog/controllers/RegistrationController.java
@@ -1,14 +1,16 @@
 package com.reglog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RegistrationController
 {
     @GetMapping("/register")
-    public String doRegistration()
+    public String doRegistration(Model model)
     {
+        model.addAttribute("Title", "Register");
         return "registration";
     }
 
