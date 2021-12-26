961b6a010f8ebbc610bd1116be2ec9f5176faab2
Musab Momin <musab.momin@kapso.in>
2021-12-27 00:19:35 +0530

    spring security with dao auth is working custom login form is pending

---
+++ src/main/java/com/reglog/security/AppUserDetails.java
@@ -0,0 +1,55 @@
+package com.reglog.security;
+
+
+import com.reglog.entity.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+public class AppUserDetails implements UserDetails
+{
+
+    private User appUser;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities()
+    {
+        SimpleGrantedAuthority authorities = new SimpleGrantedAuthority(appUser.getRole());
+        return List.of(authorities);
+    }
+
+    @Override
+    public String getPassword() {
+        return appUser.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return appUser.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
